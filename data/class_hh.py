f82166d10b5db1829419baa128c20eac7315a526
Pchel <golovkov_1994@mail.ru>
2023-07-03 14:54:12 +0300

    функция для нормальной фильтрации ЗП

--- data/class_hh.py
+++ data/class_hh.py
@@ -5,3 +5,27 @@ from data.abs_class import Api
 import requests
 
 
+class HH_api(Api):
+    def __init__(self, keyword, page=0):
+        self.par= {
+            'text': keyword,
+            'page': page}
+
+
+    def get_api(self):
+        data = requests.get("https://api.hh.ru/vacancies", self.par).json()
+        all_vac = []
+        for i in data['items']:
+            if i['salary'] != None:
+                if i['salary']['currency'] == 'RUR':
+                    all_vac.append(i)
+
+        for i in all_vac:  # Замена значения зп None на 0 для фильтрации.
+            if i['salary']['from'] is None:
+                i['salary']['from'] = 0
+
+        all_vac.sort(key=lambda x: x['salary']['from'], reverse=True)
+        return all_vac
+
+
+    def hh_save_
