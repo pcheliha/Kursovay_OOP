0452881a204a2b4a63c3f42708edf2124e587a73
Pchel <golovkov_1994@mail.ru>
2023-07-04 22:30:34 +0300

    работа с файлами.json

--- data/working_with_files.py
+++ data/working_with_files.py
@@ -2,26 +2,23 @@ import json
 
 
 class Working_with_files:
-    '''
+    """
     работа с файлами.json
-    '''
-
+    """
 
     @staticmethod
     def open_file(file_name):
-        '''
+        """
         метод для открытия файла. принимает путь и имя для файла
-
-        '''
-        with open (file_name, 'r', encoding='utf-8') as file:
+        """
+        with open(file_name, 'r', encoding='utf-8') as file:
             return json.load(file)
-
 
     @staticmethod
     def add_vac(file_name, data):
-        '''
+        """
         метод для записи файла. принимает путь и имя для файла
+        """
 
-        '''
-
-
+        with open(file_name, 'w', encoding='utf-8') as file:
+            file.write(json.dumps(data))
